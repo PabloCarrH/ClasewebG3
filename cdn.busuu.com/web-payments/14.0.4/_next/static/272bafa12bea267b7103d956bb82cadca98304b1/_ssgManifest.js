f5b8fbef04af6ed09bd3ef1e33217d7d2c52296c
Santi-Martinez-09 <santiagoo-martinezh@unilibre.edu.co>
2024-09-09 03:29:01 -0500

    Add files via upload

---
+++ cdn.busuu.com/web-payments/14.0.4/_next/static/272bafa12bea267b7103d956bb82cadca98304b1/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F404","\u002Fcancellation-reason","\u002Fcancellation-success","\u002Fcheckout","\u002Ffree-trial-info"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
