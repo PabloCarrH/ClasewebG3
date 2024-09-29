b78e9900f54e4c648f047ec698e22d8a467ddf02
Nicmoreno04 <nicolas-morenoa@unilibre.edu.co>
2024-09-29 13:51:28 -0500

    Add files via upload

---
+++ index_files/paisa.js
@@ -0,0 +1,25 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.carousel-item');
+const totalSlides = slides.length;
+
+function showSlide(slideIndex) {
+    slides.forEach((slide, index) => {
+        slide.classList.remove('active');
+        if (index === slideIndex) {
+            slide.classList.add('active');
+        }
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+    showSlide(currentSlide);
+}
+
+// Iniciar el carrusel con el primer slide
+showSlide(currentSlide);
