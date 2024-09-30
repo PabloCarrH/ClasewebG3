79500fd513ed360716bada47f55e99fc404452f3
juandavid1503 <166643564+juandavid1503@users.noreply.github.com>
2024-09-29 22:59:52 -0500

    .

--- index_files/Caleño.js
+++ index_files/Caleño.js
@@ -1,25 +1,17 @@
 let currentSlide = 0;
 const slides = document.querySelectorAll('.carousel-item');
-const totalSlides = slides.length;
 
-function showSlide(slideIndex) {
-    slides.forEach((slide, index) => {
-        slide.classList.remove('active');
-        if (index === slideIndex) {
-            slide.classList.add('active');
-        }
-    });
+function showSlide(index) {
+    slides.forEach(slide => slide.classList.remove('active'));
+    slides[index].classList.add('active');
 }
 
 function nextSlide() {
-    currentSlide = (currentSlide + 1) % totalSlides;
+    currentSlide = (currentSlide + 1) % slides.length;
     showSlide(currentSlide);
 }
 
 function prevSlide() {
-    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
     showSlide(currentSlide);
 }
-
-// Iniciar el carrusel con el primer slide
-showSlide(currentSlide);
