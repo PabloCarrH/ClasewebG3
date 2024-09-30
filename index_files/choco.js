4e4d182b359fb985fb73164ce8c9ce42c0facf19
juandavid1503 <166643564+juandavid1503@users.noreply.github.com>
2024-09-30 14:53:05 -0500

    choco

---
+++ index_files/choco.js
@@ -0,0 +1,21 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.carousel-item');
+
+function showSlide(slideIndex) {
+    slides.forEach((slide, index) => {
+        slide.classList.remove('active');
+        if (index === slideIndex) {
+            slide.classList.add('active');
+        }
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+}
